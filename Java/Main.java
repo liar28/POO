0f502637f7d187ecf4cf841b6df5cd39cf3955e8
liar28 <santiago.ordaz@alumnos.udg.mx>
2022-06-16 01:03:52 -0500

    Ni pta idea que acabo de hacer

--- Java/Main.java
+++ Java/Main.java
@@ -1,6 +1,13 @@
 class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        Car car = new Car("AMQ123", new Account("Andres Herrera", "AND123"));
+        car.passenger = 4;
+        car.printDataCar();
+
+        Car car2 = new Car("QWE567", new Account("Andrea Herrera", "ANDA876"));
+        car2.passenger = 3;
+        car2.printDataCar();
     }
     
 }
